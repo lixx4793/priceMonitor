7459f39190c19d508f35bd1ab08fe2308dfd8d25
lixx4793 <>
2020-12-17 19:08:25 -0800

    add static page server

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const config = require('dotenv').config({path: __dirname + "/.env"});
 const port = config.parsed.PORT || 8080;
 var bodyParser = require('body-parser')
-const app = express();
 const scripHandlerRouter = require("./src/scripHandler");
 const scripManagerRouter = require("./src/scripManager");
 const scripAuthendication = require("./src/authendication");
 const accountManager = require("./src/accountManager");
+const serveStatic = require('serve-static');
+const app = express();
 
-
-
+app.use(serveStatic(path.join(__dirname, './build'), {
+  maxAge: '1d'
+}));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
